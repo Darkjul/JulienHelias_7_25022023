da13909132103770afe57d15f60e6d5db6990730
Julien Helias <julien.helias@free.fr>
2023-03-09 21:28:36 +0100

    Création Pages et mise en places des premières routes avec index.html

---
+++ src/pages/error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            <h1>Erreur 404 ! La page que vous demandez est introuvable.</h1>
+        </div>
+    );
+};
+
+export default Error;
