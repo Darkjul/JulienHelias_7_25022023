9b4e2f1fed59257e2bb4d5b123464a86b579ae52
Julien Helias <julien.helias@free.fr>
2023-04-20 21:52:37 +0200

    Simplification boucle du Slide

--- src/components/Carrousel/Carrousel.jsx
+++ src/components/Carrousel/Carrousel.jsx
@@ -6,61 +6,39 @@ import arrowright from "../../assets/chevron_carousel_right.png";
 
 const Carrousel = ({ slides }) => {
 	
-	const [current, setCurrent] = useState(0); // On définit l'index du premier slide à 0
-	const length = slides.length;
+	const [current, setCurrent] = useState(0); // On définit l'index du premier slide à 0	
 
 	const nextSlide = () => {
-		setCurrent(current === length - 1 ? 0 : current + 1); // On repartira au premier slide quand on arrive au dernier
+		setCurrent(current === slides.length - 1 ? 0 : current + 1); // On repartira au premier slide quand on arrive au dernier
 	};
 
 	const prevSlide = () => {
-		setCurrent(current === 0 ? length - 1 : current - 1); // On repartira au dernier slide quand on est au premier
+		setCurrent(current === 0 ? slides.length - 1 : current - 1); // On repartira au dernier slide quand on est au premier
 	};
 
 	return (
-		<div>
-	<section id="carrousel-container">
-	  {length > 1 && ( // Affichage des flèches seulement si la longueur est > 1
-	    <img
-	      src={arrowleft}
-	      alt="Flèche gauche"
-	      onClick={prevSlide}
-	      className="leftArrow"
-	    />
-	  )}
 
-	  {length > 1 && ( // Affichage des flèches seulement si la longueur est > 1
-	    <img
-	      src={arrowright}
-	      alt="Flèche droite"
-	      onClick={nextSlide}
-	      className="rightArrow"
-	    />
-	  )}
-
-	  {slides.map(
-	    (
-	      slide,
-	      index // Lancement slider avec affichage conditionnel et opacité quand le slide en cours vaut l'index
-	    ) => (
-	      <div
-	        key={index}
-	        className={current === index ? "slider active-anim" : "slider "}
-	      >
-	        {index === current && (
-	          <img src={slide} alt="Images de logements à louer" />
-	        )}
-	        {index === current && (
-	          <span className="slider__number">
-	            {current + 1}/{length}
-	          </span>
-	        )}
-	      </div>
-	    )
-	  )}
-			</section>
-			<img src={slides[current]} alt="Images de logements à louer" />
-		</div>
+		<section style={{backgroundImage : `url(${slides[current]})`}} id="carrousel-container">
+            {slides.length > 1 && 
+                <>
+                    <img 
+                        className="leftArrow"
+                        src={arrowleft} 
+                        alt="Flèche gauche" 
+                        onClick={prevSlide}
+                    />
+					
+                    <img 
+                        className="rightArrow"
+                        src={arrowright} 
+                        alt="Flèche droite" 
+                        onClick={nextSlide}
+                    />
+					
+                    <p className='slideCount'> {current + 1} / {slides.length}</p>
+                </>
+            } 
+        </section>
 	);
 	
 }
